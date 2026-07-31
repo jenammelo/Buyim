d6d3c3a37c9c8227d69a2a8d25657a460f14d159
Teba <tebabrightco@gmail.com>
2026-07-31 14:44:20 +0100

    inngest function updated

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,17 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import {
-  syncUserCreation,
-  syncUserUpdate,
-  syncUserDeletion,
-} from "@/inngest/functions";
+import { syncUserCreation } from "@/inngest/functions";
 
 // Next.js App Router expects GET, POST, and PUT exported directly
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
-    syncUserUpdate,
-    syncUserDeletion,
   ],
 });
+  
